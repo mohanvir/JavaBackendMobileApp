d42a5cfab52f02042d4624144ddfbee9c2d6f11a
Mohanvir <mohanvir.gill@Mohanvirs-MacBook-Pro-2.local>
2020-12-27 18:28:28 -0800

    inital setup of one Post request

---
+++ src/main/java/com/appsdeveloperblog/app/ws/UserRepository.java
@@ -0,0 +1,12 @@
+package com.appsdeveloperblog.app.ws;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.appsdeveloperblog.app.ws.enitity.UserEntity;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+//	want each user to be unique by email address
+	UserEntity findByEmail(String email);
+}
